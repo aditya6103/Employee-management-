dcf2fe7dbe0d4ca1c2ced83e801d7f8365b3eeba
Aditya <adityaujjawal99@gmail.com>
2026-07-04 20:17:45 +0530

    Find By id Employee

---
+++ employee-management/src/main/java/com/wozo/employee_management/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.wozo.employee_management.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+   public ResourceNotFoundException(String message)
+    {
+        super(message);
+    }
+
+}

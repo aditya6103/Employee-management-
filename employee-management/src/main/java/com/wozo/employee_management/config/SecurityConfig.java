27c5543b58733d8dd36beb6508f06f6737e00172
Aditya <adityaujjawal99@gmail.com>
2026-07-27 16:16:59 +0530

    Implement Spring Security with InMemory authentication and authority-based authorization

---
+++ employee-management/src/main/java/com/wozo/employee_management/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.wozo.employee_management.config;
+
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+       @Bean
+     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+           http
+                   .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                           .requestMatchers("/Employees","/Employees/**").hasAuthority("EMPLOYEE_READ")
+                           .requestMatchers("/departments","/departments/**").hasAuthority("DEPARTMENT_READ")
+                           .anyRequest().authenticated()
+                   )
+
+                   // This line resists 403 Forbidden error and pop-up default login page
+                   .formLogin(Customizer.withDefaults());
+
+           return http.build();
+
+
+      }
+
+      @Bean
+      public UserDetailsService userDetailsService()
+      {
+          UserDetails user= User.builder()
+                  .username("Aditya")
+                  .password("{noop}12345")
+                  .authorities("EMPLOYEE_READ")
+                  .build();
+          UserDetails admin= User.builder()
+                  .username("Admin")
+                  .password("{noop}123")
+                  .authorities( "EMPLOYEE_READ",
+                          "EMPLOYEE_WRITE",
+                          "EMPLOYEE_DELETE",
+                          "DEPARTMENT_READ",
+                          "DEPARTMENT_WRITE")
+                  .build();
+
+          return new InMemoryUserDetailsManager(user,admin);
+
+      }
+
+
+
+
+
+}
--- employee-management/src/main/java/com/wozo/employee_management/repository/DepartmentRepository.java
+++ employee-management/src/main/java/com/wozo/employee_management/repository/DepartmentRepository.java
@@ -3,6 +3,7 @@ package com.wozo.employee_management.repository;
 import com.wozo.employee_management.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
 
 
